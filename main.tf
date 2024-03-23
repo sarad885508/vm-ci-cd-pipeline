d576d977bf8be1be96c8ec3e5f7900d6f6b63935
sarad885508 <sarad8855@gmail.com>
2024-03-23 12:55:00 +0530

    initial code

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "vm-instance" {
+  name         = "ci-cd-workflow"
+  machine_type = "e2-medium"
+  zone    = "us-west4-b"
+  project = "compact-record-414005"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-10-buster-v20240110"
+    }
+  }
+ network_interface {
+    network = "default"
+    access_config {
+    }
+ }
+}
